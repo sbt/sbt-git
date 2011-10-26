f8e4b2b06c2621464455998e5a8888a256b5ce1f
Josh Suereth <joshua.suereth@gmail.com>
2011-10-26 16:06:48 -0400

    JGit now forks.  Lots more friendly for bad git commands

--- src/main/scala/com/typesafe/git/JGitRunner.scala
+++ src/main/scala/com/typesafe/git/JGitRunner.scala
@@ -6,9 +6,18 @@ import org.eclipse.jgit.pgm.{Main=>JGit}
 
 /** A mechanism to run GIT using the pure java JGit implementation. */
 object JGitRunner extends GitRunner {
-  override def apply(args: String*)(cwd: File, log: Logger): Unit =
+  override def apply(args: String*)(cwd: File, log: Logger = ConsoleLogger()): Unit = {
     // TODO -  Can we just assume the .git repo? I hope so....
-    JGit.main((Seq("--git-dir", cwd.getAbsolutePath + "/.git") ++ args).toArray)  
+    //JGit.main((Seq("--git-dir", cwd.getAbsolutePath + "/.git") ++ args).toArray)  
+    // Make a good ole fashioned classpath.
+    getClass.getClassLoader match {
+       case cl: java.net.URLClassLoader =>
+         val cp = cl.getURLs map (_.getFile) mkString ":"
+         Fork.java(None, Seq("-classpath", cp, "org.eclipse.jgit.pgm.Main") ++ args, Some(cwd), log)         
+       case _ => log.error("Could not find classpath for JGit!")
+    }
+
+  }
   override def toString = "jgit"  
 }
 

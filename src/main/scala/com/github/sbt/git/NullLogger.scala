6d5b9120e1be77902b68f455b0a19f31fddd8bdc
Billy Autrey <40704452+BillyAutrey@users.noreply.github.com>
2024-10-07 15:59:26 -0500

    Clean up deprecation of unicode arrow

--- src/main/scala/com/github/sbt/git/NullLogger.scala
+++ src/main/scala/com/github/sbt/git/NullLogger.scala
@@ -5,9 +5,9 @@ import sbt.Level
 import sbt.ControlEvent
 
 object NullLogger extends sbt.BasicLogger {
-  override def control(event: ControlEvent.Value, message: ⇒ String): Unit = ()
-  override def log(level: Level.Value, message: ⇒ String): Unit = ()
+  override def control(event: ControlEvent.Value, message: => String): Unit = ()
+  override def log(level: Level.Value, message: => String): Unit = ()
   override def logAll(events: Seq[LogEvent]): Unit = ()
-  override def success(message: ⇒ String): Unit = ()
-  override def trace(t: ⇒ Throwable): Unit = ()
+  override def success(message: => String): Unit = ()
+  override def trace(t: => Throwable): Unit = ()
 }
